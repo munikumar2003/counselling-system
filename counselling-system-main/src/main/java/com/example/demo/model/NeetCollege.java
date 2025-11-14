1d5ee4f9c3635665c072ee95c6fd36ed853302d5
munikumar2003 <munikumarchemuru2003@gmail.com>
2025-11-14 21:35:10 +0530

    added cutoff for neet

--- counselling-system-main/src/main/java/com/example/demo/model/NeetCollege.java
+++ counselling-system-main/src/main/java/com/example/demo/model/NeetCollege.java
@@ -12,7 +12,9 @@ import java.util.List;
 public class NeetCollege {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
+
     private String name;
     private String location;
     private String type;
