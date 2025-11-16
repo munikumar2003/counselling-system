2343d0a1e1525163934e8afb5509c79e659cb1fb
munikumar2003 <munikumarchemuru2003@gmail.com>
2025-11-16 21:04:25 +0530

    modified demo application

--- counselling-system-main/src/main/java/com/example/demo/DemoApplication.java
+++ counselling-system-main/src/main/java/com/example/demo/DemoApplication.java
@@ -8,20 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
-
-  @Value("${NAME:World}")
-  String name;
-
-  @RestController
-  class HelloworldController {
-    @GetMapping("/")
-    String hello() {
-      return "Hello " + name + "!";
-    }
-  }
-
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
   }
-
 }
